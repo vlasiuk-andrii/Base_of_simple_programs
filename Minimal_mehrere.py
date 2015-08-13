0ed3b48d2ac95d15a2d4c69f4593c587e0ed22c4
vlasiuk-andrii <vlasiuk.andrii93@gmail.com>
2015-08-13 11:26:55 +0300

    Final version

--- Minimal_mehrere.py
+++ Minimal_mehrere.py
@@ -1,9 +1,12 @@
-a = int(input())
-b = int(input())
+a = int(input('Insert a: '))
+b = int(input('Insert b: '))
 d = 0
-while 1:
-    d+=1
-    if (d%a == 0) and (d%b == 0):
-        print(d)
-        break
+if a == 0 or b ==0:
+    print('Zero values are not allowed')
+else:    
+    while 1:
+        d+=1
+        if (d%a == 0) and (d%b == 0):
+            print('Minimal mehrere=', d)
+            break
 
